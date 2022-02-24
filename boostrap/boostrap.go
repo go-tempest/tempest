dd01f202a5cb028a67be5914b25ec06a017f58f3
liuxiaxin <lxxwilliam@163.com>
2022-02-24 14:30:50 +0800

    add log

--- boostrap/boostrap.go
+++ boostrap/boostrap.go
@@ -1,50 +1,53 @@
 package boostrap
 
 import (
-    "flag"
-    "fmt"
-    "github.com/go-tempest/tempest/config"
-    "github.com/go-tempest/tempest/register"
-    "github.com/spf13/viper"
-    "os"
-    "sync"
+	"flag"
+	"fmt"
+	"github.com/go-tempest/tempest/config"
+	"github.com/go-tempest/tempest/log"
+	"github.com/go-tempest/tempest/register"
+	"github.com/spf13/viper"
+	"os"
+	"sync"
 )
 
 type bootstrap struct {
-    sync.Once
+	sync.Once
 }
 
 func (b *bootstrap) start() {
-    b.Do(func() {
-        new(register.Registration).StartIfNecessary()
-        // TODO 启动
-    })
+	b.Do(func() {
+		new(register.Registration).StartIfNecessary()
+		// TODO 启动
+	})
 }
 
 func getConfigPath() *string {
-    var configPath = flag.String("c", "", "Specifies the path used to search the configuration file")
-    flag.Parse()
-    return configPath
+	var configPath = flag.String("c", "", "Specifies the path used to search the configuration file")
+	flag.Parse()
+	return configPath
 }
 
 func parseYaml(v *viper.Viper) {
-    if err := v.Unmarshal(&config.TempestCfg); err != nil {
-        fmt.Println("初始化配置失败") // TODO 后续替换成通用日志组件
-        os.Exit(-1)
-    }
+	if err := v.Unmarshal(&config.TempestCfg); err != nil {
+		fmt.Println("初始化配置失败") // TODO 后续替换成通用日志组件
+		os.Exit(-1)
+	}
 }
 
 func init() {
-    initViper()
-    new(bootstrap).start()
+	initViper()
+	log.InitLogger()  //初始化日志
+	defer log.Flush() //刷新日志
+	new(bootstrap).start()
 }
 
 func initViper() {
-    viper.AutomaticEnv()
-    viper.SetConfigType(config.DefaultConfigType)
-    viper.SetConfigName(config.DefaultConfigName)
-    viper.AddConfigPath(*getConfigPath())
+	viper.AutomaticEnv()
+	viper.SetConfigType(config.DefaultConfigType)
+	viper.SetConfigName(config.DefaultConfigName)
+	viper.AddConfigPath(*getConfigPath())
 
-    v := viper.GetViper()
-    parseYaml(v)
+	v := viper.GetViper()
+	parseYaml(v)
 }
--- go.mod
+++ go.mod
@@ -37,4 +37,5 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/natefinch/lumberjack v2.0.0+incompatible
 )
